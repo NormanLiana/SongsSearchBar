0398b6eb3ec365a865d306503cc53d8718f467b0
NormanLiana <liananorman@pursuit.org>
2019-08-15 16:34:22 -0400

    adds outlets in detail VC for image, song and artist

---
+++ SongsTableViewSearchBar/DetailSongViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailSongViewController.swift
+//  SongsTableViewSearchBar
+//
+//  Created by Liana Norman on 8/15/19.
+//  Copyright © 2019 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class DetailSongViewController: UIViewController {
+
+    
+    @IBOutlet weak var detailSongArtistOutlet: UILabel!
+    @IBOutlet weak var detailSongTitleOutlet: UILabel!
+    @IBOutlet weak var detailImageOutlet: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
